682001ff73baf7fce0e0ed471baff2501ba61b4e
fatemeh-sabzevari <sabzevarifatemeh0188@gmail.com>
2021-08-26 22:40:46 +0330

    delete

--- bookstore/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from accounts.models import BaseUser
-
-# Register your models here.
-
-
-admin.site.register(BaseUser)
--- bookstore/cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-# Register your models here.
-from cart.models import CartItem, OrderCart, Address
-
-admin.site.register(CartItem)
-admin.site.register(OrderCart)
-admin.site.register(Address)
